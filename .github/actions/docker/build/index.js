2488df0eafe44c240c0d0386e6c26f2719c431b4
infracoder7 <sradhu.geek@gmail.com>
2022-04-19 14:38:34 +0530

    adding ghh actions

---
+++ .github/actions/docker/build/index.js
@@ -0,0 +1,32 @@
+const core = require(@actions/core);
+const exec = require(@actions/exec);
+
+async function run() {
+    try {
+        let name = core.getInput('name');
+        let tag = core.getInput('tag');
+        let fulltag = `${name}:${tag}`;
+        const work_dir = core.getInput('working-directory')
+        if (! work_dir) {
+            work_dir = '.'
+        }
+        await exec.exec('docker build --tag',
+        fulltag,
+        '--file',
+        core.getInput('dockerfile'),
+        work_dir
+        );
+        isPushRequired = core.getInput('push');
+        if (push) {
+            await exec.exec('docker push',
+            fulltag
+            );  
+        }
+    }
+    catch (err) {
+        core.setFailed(err.message);
+    }
+}
+
+run();
+
---
+++ .github/actions/docker/login/index.js
@@ -0,0 +1,20 @@
+const core = require(@actions/core);
+const exec = require(@actions/exec);
+
+async function run() {
+    try {
+        await exec.exec('docker',[
+            'login',
+            '--username',
+            core.getInput('username'),
+            '--password',
+            core.getInput('password')
+        ]);
+    }
+    catch (err) {
+        core.setFailed(err.message);
+    }
+}
+
+run();
+
